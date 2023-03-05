7a274ebe4819670885b85e0ab64c7e91acac5c1b
Madaga95 <amathgueye95@gmail.com>
2023-03-05 20:43:31 +0100

    Validation mail

--- controllers/Utilisateur/Utilisateur.controller.php
+++ controllers/Utilisateur/Utilisateur.controller.php
@@ -2,35 +2,40 @@
 require_once("./controllers/MainController.controller.php");
 require_once("./models/Utilisateur/Utilisateur.model.php");
 
-class UtilisateurController extends MainController{
+class UtilisateurController extends MainController
+{
     private $utilisateurManager;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->utilisateurManager = new UtilisateurManager();
     }
 
-    public function validation_login($login,$password){
-        if($this-> utilisateurManager->isCombinaisonValide($login, $password)){
+    public function validation_login($login, $password)
+    {
+        if ($this->utilisateurManager->isCombinaisonValide($login, $password)) {
             // verifie si le compte est valide
-            if($this->utilisateurManager->estCompteActive($login)){
+            if ($this->utilisateurManager->estCompteActive($login)) {
                 Toolbox::ajouterMessageAlerte("Bon retour sur le site", Toolbox::COULEUR_VERTE);
                 $_SESSION['profil'] = [
                     'login' => $login,
                 ];
-                header('Location: '.URL.'compte/profil');
-                
-            }else{
-                Toolbox::ajouterMessageAlerte("Votre compte " . $login . " n'a pas été activé par mail", Toolbox::COULEUR_ORANGE);
-                header('Location: '.URL.'login');
+                header('Location: ' . URL . 'compte/profil');
+            } else {
+                $msg = "Votre compte " . $login . " n'a pas été activé par mail.";
+                $msg .= " <a href='renvoyerMailValidation/" . $login . "'>Renvoyer le mail de validation</a>";
+                Toolbox::ajouterMessageAlerte($msg, Toolbox::COULEUR_ORANGE);
+                header('Location: ' . URL . 'login');
             }
-
-        }else{
+        } else {
             Toolbox::ajouterMessageAlerte("Combinaison login/mot de passe invalide", Toolbox::COULEUR_ROUGE);
-            header('Location: '.URL.'login');
+            header('Location: ' . URL . 'login');
         }
     }
 
-    public function profil(){
+    // gestion de profil
+    public function profil()
+    {
         $datas = $this->utilisateurManager->getUserInformation($_SESSION['profil']['login']);
         $_SESSION['profil']['role'] = $datas['role'];
 
@@ -44,29 +49,49 @@ class UtilisateurController extends MainController{
         $this->genererPage($data_page);
     }
 
-    public function deconnexion(){
+    public function deconnexion()
+    {
         session_destroy();
-        header('Location: '.URL.'accueil');
+        header('Location: ' . URL . 'accueil');
     }
 
-    public function validation_creerCompte($login, $mail, $password){
-        if($this->utilisateurManager->verifLoginDisponible($login)){
+    public function validation_creerCompte($login, $mail, $password)
+    {
+        if ($this->utilisateurManager->verifLoginDisponible($login)) {
             $passwordCrypte = password_hash($password, PASSWORD_DEFAULT);
             $clef = rand(0, 9999);
-            if($this->utilisateurManager->bdCreerCompte($login, $mail, $passwordCrypte, $clef)){
+            if ($this->utilisateurManager->bdCreerCompte($login, $mail, $passwordCrypte, $clef)) {
+                $this->sendMailValidation($login, $mail, $clef);
                 Toolbox::ajouterMessageAlerte("Votre compte a bien été créé, vous allez recevoir un mail pour l'activer", Toolbox::COULEUR_VERTE);
-                header('Location: '.URL.'login');
-            }else{
+                header('Location: ' . URL . 'login');
+            } else {
                 Toolbox::ajouterMessageAlerte("Erreur lors de la création du compte", Toolbox::COULEUR_ROUGE);
-                header('Location: '.URL.'creerCompte');
+                header('Location: ' . URL . 'creerCompte');
             }
-        }else{
+        } else {
             Toolbox::ajouterMessageAlerte("Le login " . $login . " est déjà utilisé", Toolbox::COULEUR_ROUGE);
-            header('Location: '.URL.'creerCompte');
+            header('Location: ' . URL . 'creerCompte');
         }
     }
-    
-    public function pageErreur($msg){
+
+    // envoi d'email
+    private function sendMailValidation($login, $mail, $clef)
+    {
+        $urlVerification = URL . "validationMail/" . $login . "/" . $clef;
+        $sujet = "Création du compte sur le site xxx";
+        $message = "Pour valider votre compte veuillez cliquer sur le lien suivant " . $urlVerification;
+        Toolbox::sendMail($mail, $sujet, $message);
+    }
+
+    // renvoi de mail de validation
+    public function renvoyerMailValidation($login){
+        $utilisateur = $this->utilisateurManager->getUserInformation($login);
+        $this->sendMailValidation($login,$utilisateur['mail'],$utilisateur['clef']);
+        header("Location: ".URL."login" );
+    }
+
+    public function pageErreur($msg)
+    {
         parent::pageErreur($msg);
     }
 }
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-define("URL", str_replace("index.php","",(isset($_SERVER['HTTPS'])? "https" : "http").
-"://".$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"]));
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") .
+    "://" . $_SERVER['HTTP_HOST'] . $_SERVER["PHP_SELF"]));
 
 require_once("./controllers/Visiteur/Visiteur.controller.php");
 require_once("./controllers/Securite.class.php");
@@ -12,59 +12,72 @@ $visiteurController = new VisiteurController();
 $utilisateurController = new UtilisateurController();
 
 try {
-    if(empty($_GET['page'])){
+    if (empty($_GET['page'])) {
         $page = "accueil";
     } else {
-        $url = explode("/", filter_var($_GET['page'],FILTER_SANITIZE_URL));
+        $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
         $page = $url[0];
     }
 
-    switch($page){
-        case "accueil" : $visiteurController->accueil();
-        break;
-        case "login" : $visiteurController->login();
-        break;
-        case "validation_login" :
-            if(!empty($_POST['login']) && !empty($_POST['password'])){
+    switch ($page) {
+        case "accueil":
+            $visiteurController->accueil();
+            break;
+        case "login":
+            $visiteurController->login();
+            break;
+        case "validation_login":
+            if (!empty($_POST['login']) && !empty($_POST['password'])) {
                 $login = Securite::secureHTML($_POST['login']);
                 $password = Securite::secureHTML($_POST['password']);
                 $utilisateurController->validation_login($login, $password);
-            }else{
+            } else {
                 Toolbox::ajouterMessageAlerte("Login ou mot de passe non renseigné", Toolbox::COULEUR_ROUGE);
                 // effectuer redirection vers la page de login
-                header('Location: '.URL.'login');
+                header('Location: ' . URL . 'login');
             }
-        break;
-        case "creerCompte" : $visiteurController->creerCompte();
-        break;
-        case "validation_creerCompte" : 
-            if(!empty($_POST['login']) && !empty($_POST['mail']) && !empty($_POST['password'])){
+            break;
+        case "creerCompte":
+            $visiteurController->creerCompte();
+            break;
+        case "validation_creerCompte":
+            if (!empty($_POST['login']) && !empty($_POST['mail']) && !empty($_POST['password'])) {
                 $login = Securite::secureHTML($_POST['login']);
                 $mail = Securite::secureHTML($_POST['mail']);
                 $password = Securite::secureHTML($_POST['password']);
                 $utilisateurController->validation_creerCompte($login, $mail, $password);
-            }else{
+            } else {
                 Toolbox::ajouterMessageAlerte("Tous les champs doivent être renseignés", Toolbox::COULEUR_ROUGE);
                 // effectuer redirection vers la page de création de compte
-                header('Location: '.URL.'creerCompte');
+                header('Location: ' . URL . 'creerCompte');
             }
-        break;
-        case "compte" :
+            break;
+        case 'renvoyerMailValidation': $utilisateurController->renvoyerMailValidation($url[1]);
+            break;
+        case 'validationMail':
+            echo "test";
+            # code...
+            break;
+        case "compte":
             // faire la vérifation de l'authentification
-            if(!Securite::estConnecte()){
+            if (!Securite::estConnecte()) {
                 Toolbox::ajouterMessageAlerte("Vous devez être connecté pour accéder à cette page", Toolbox::COULEUR_ROUGE);
-                header('Location: '.URL.'login');
-            }else{
-                switch($url[1]){
-                    case "profil": $utilisateurController->profil();
-                    break;
-                    case "deconnexion" : $utilisateurController->deconnexion();
-                    default : throw new Exception("La page n'existe pas");
+                header('Location: ' . URL . 'login');
+            } else {
+                switch ($url[1]) {
+                    case "profil":
+                        $utilisateurController->profil();
+                        break;
+                    case "deconnexion":
+                        $utilisateurController->deconnexion();
+                    default:
+                        throw new Exception("La page n'existe pas");
                 }
-            } 
-        break;
-        default : throw new Exception("La page n'existe pas");
+            }
+            break;
+        default:
+            throw new Exception("La page n'existe pas");
     }
-} catch (Exception $e){
+} catch (Exception $e) {
     $visiteurController->pageErreur($e->getMessage());
 }
